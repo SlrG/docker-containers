1d26730f0897c8e73927ac6579c7cb90ac4ba895
Michael wehr <michael_wehr@web.de>
2015-03-30 17:36:46 +0200

    modify service init

--- glftpd/glftpd.sh
+++ glftpd/glftpd.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Fix timezone problems
-if [[ $(cat /etc/timezone) != $TZ ]] ; then
-	echo "$TZ" > /etc/timezone
-	dpkg-reconfigure -f noninteractive tzdata
-fi
-
-# Rsync not existing glftpd files to volume host folder
-rsync -avhr --ignore-existing /glftpd-inst/ /glftpd/
-chown -R nobody:users /glftpd
-
 # Start glfpd
-service xinetd start
+if ! pid=$(pgrep xinetd);
+then
+	service xinetd start
+fi
