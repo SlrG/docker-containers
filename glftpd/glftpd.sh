ac3d23245159a8a117e56011fe61e32197339439
Michael wehr <michael_wehr@web.de>
2015-03-30 11:22:28 +0200

    add fix for timezone problems

--- glftpd/glftpd.sh
+++ glftpd/glftpd.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Fix timezone problems
+if [[ $(cat /etc/timezone) != $TZ ]] ; then
+	echo "$TZ" > /etc/timezone
+	dpkg-reconfigure -f noninteractive tzdata
+fi
+
+# Rsync not existing glftpd files to volume host folder
 rsync -avhr --ignore-existing /glftpd-inst/ /glftpd/
 chown -R nobody:users /glftpd
+
+# Start glfpd
 service xinetd start
